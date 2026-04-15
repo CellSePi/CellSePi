4ec281e998797270e1f043dc32e92cf8de0ae002
Jenna <Jennaahlvers@t-online.de>
2026-04-15 23:23:09 +0200

    -adapted hex conversion to color picker -fixed slider movement -fixed color display in the menu

--- src/cellsepi/frontend/main_window/gui_colors.py
+++ src/cellsepi/frontend/main_window/gui_colors.py
@@ -18,7 +18,10 @@ def hex_to_rgb(hex_color):
         rgb_color (tuple)
     """
     hex_color = hex_color.lstrip("#")
-    return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+    if len(hex_color) == 8:  # AARRGGBB
+        hex_color = hex_color[2:]  # cut the aplha channel
+
+    return tuple(int(hex_color[i:i + 2], 16) for i in (0, 2, 4))
 
 def rgb_to_hex(rgb_color):
     """
@@ -28,7 +31,7 @@ def rgb_to_hex(rgb_color):
     Returns:
         hex_color (str)
     """
-    return f"#{rgb_color[0]:02x}{rgb_color[1]:02x}{rgb_color[2]:02x}"#"#{:02x}{:02x}{:02x}".format(*rgb_color)
+    return "#{:02x}{:02x}{:02x}".format(*rgb_color)
 
 class ColorTypes(Enum):
     Mask = 1
@@ -40,8 +43,10 @@ class ColorSelection:
         self.gui = gui
         color_mask = rgb_to_hex(self.config.get_mask_color())
 
+
         color_outline = rgb_to_hex(self.config.get_outline_color())
-        self.color_picker = ColorPicker(color=color_mask, width=300)
+        self.current_color = None
+        self.color_picker = ColorPicker(on_color_change=self.on_color_change)
         #self.color_picker.hex.border_color=ft.Colors.BLUE_ACCENT
         #elf.color_picker.r.border_color=ft.Colors.BLUE_ACCENT
         #self.color_picker.g.border_color=ft.Colors.BLUE_ACCENT
@@ -60,16 +65,22 @@ class ColorSelection:
         )
     def open_color_picker_mask(self,e):
         self.dialog.open()
-        self.color_picker.color = rgb_to_hex(self.config.get_mask_color())
+        #self.color_picker.color = rgb_to_hex(self.config.get_mask_color())
+        self.current_color = None
         self.color_type = ColorTypes.Mask
         e.control.page.update()
 
 
     def open_color_picker_outline(self, e):
         self.dialog.open()
-        self.color_picker.color = rgb_to_hex(self.config.get_outline_color())
+        #self.color_picker.color = rgb_to_hex(self.config.get_outline_color())
+        self.current_color = None
         self.color_type = ColorTypes.Outline
         e.control.page.update()
+
+    def on_color_change(self,e):
+        self.current_color = e.data
+        print("on color change", e.data)
 
     def change_color(self, e):
         """
@@ -78,17 +89,18 @@ class ColorSelection:
         Attributes:
             self.color_picker (ColorPicker)
         """
+        print("selected color",self.current_color)
         if self.color_type == ColorTypes.Mask:
-            self.color_icon_mask.icon_color = self.color_picker.colo
-            self.config.set_mask_color(hex_to_rgb(self.color_picker.color))
-            self.gui.canvas.mask_color = hex_to_rgb(self.color_picker.color)
+            self.color_icon_mask.icon_color = self.current_color
+            self.config.set_mask_color(hex_to_rgb(self.current_color))
+            self.gui.canvas.mask_color = hex_to_rgb(self.current_color)
             self.gui.canvas.update_mask_image()
             #ModuleType.REVIEW.value.mask_color = self.config.get_mask_color()
             #ModuleType.REVIEW.value.update_class()
         else:
-            self.color_icon_outline.icon_color = self.color_picker.color
-            self.config.set_outline_color(hex_to_rgb(self.color_picker.color))
-            self.gui.canvas.outline_color = hex_to_rgb(self.color_picker.color)
+            self.color_icon_outline.icon_color = self.current_color
+            self.config.set_outline_color(hex_to_rgb(self.current_color))
+            self.gui.canvas.outline_color = hex_to_rgb(self.current_color)
             self.gui.canvas.update_mask_image()
             #ModuleType.REVIEW.value.outline_color = self.config.get_outline_color()
             #ModuleType.REVIEW.value.update_class()
