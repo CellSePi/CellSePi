7b95a2e29ea7e26e7e3069e3c709123be1d2156e
Jenna <Jennaahlvers@t-online.de>
2026-04-15 13:33:52 +0200

    -fixed color picker

--- src/cellsepi/frontend/main_window/gui_colors.py
+++ src/cellsepi/frontend/main_window/gui_colors.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 
 import flet as ft
-from flet_contrib.color_picker import ColorPicker
+from flet_color_pickers import ColorPicker
 from enum import Enum
 
 from cellsepi.frontend.main_window.gui_page_overlay import PageOverlay
@@ -28,7 +28,7 @@ def rgb_to_hex(rgb_color):
     Returns:
         hex_color (str)
     """
-    return "#{:02x}{:02x}{:02x}".format(*rgb_color)
+    return f"#{rgb_color[0]:02x}{rgb_color[1]:02x}{rgb_color[2]:02x}"#"#{:02x}{:02x}{:02x}".format(*rgb_color)
 
 class ColorTypes(Enum):
     Mask = 1
@@ -39,22 +39,23 @@ class ColorSelection:
         self.config = gui.csp.config
         self.gui = gui
         color_mask = rgb_to_hex(self.config.get_mask_color())
+
         color_outline = rgb_to_hex(self.config.get_outline_color())
-        self.color_picker = ColorPicker(color=color_mask, width=430)
-        self.color_picker.hex.border_color=ft.Colors.BLUE_ACCENT
-        self.color_picker.r.border_color=ft.Colors.BLUE_ACCENT
-        self.color_picker.g.border_color=ft.Colors.BLUE_ACCENT
-        self.color_picker.b.border_color=ft.Colors.BLUE_ACCENT
+        self.color_picker = ColorPicker(color=color_mask, width=300)
+        #self.color_picker.hex.border_color=ft.Colors.BLUE_ACCENT
+        #elf.color_picker.r.border_color=ft.Colors.BLUE_ACCENT
+        #self.color_picker.g.border_color=ft.Colors.BLUE_ACCENT
+        #self.color_picker.b.border_color=ft.Colors.BLUE_ACCENT
         self.color_icon_mask = ft.IconButton(icon=ft.Icons.BRIGHTNESS_1_ROUNDED,icon_color=color_mask,disabled=True,mouse_cursor=ft.MouseCursor.CLICK)
         self.color_icon_outline = ft.IconButton(icon=ft.Icons.BRIGHTNESS_1_ROUNDED, icon_color=color_outline,disabled=True,mouse_cursor=ft.MouseCursor.CLICK)
         self.color_type = None
         self.dialog = PageOverlay(self.gui.page,ft.Stack([ft.Row([
-            ft.Column([ft.Card(content=ft.Stack([ft.Container(ft.ListTile(height=370,width=430),padding=10),ft.Container(ft.Column(
+            ft.Column([ft.Card(content=ft.Stack([ft.Container(ft.ListTile(height=370,width=650),padding=10),ft.Container(ft.Column(
                 [self.color_picker,
                 ft.Container(ft.Row([ft.TextButton("Save", on_click=self.change_color)
                  ],alignment=ft.MainAxisAlignment.END),padding=10)
                 ]
-            ),height=430,width=450,padding=10)]))],horizontal_alignment=ft.CrossAxisAlignment.CENTER,alignment=ft.MainAxisAlignment.CENTER)],alignment=ft.MainAxisAlignment.CENTER)]),
+            ),height=430,width=650,padding=10)]))],horizontal_alignment=ft.CrossAxisAlignment.CENTER,alignment=ft.MainAxisAlignment.CENTER)],alignment=ft.MainAxisAlignment.CENTER)]),
             on_dismiss=self.close_dialog,
         )
     def open_color_picker_mask(self,e):
@@ -78,7 +79,7 @@ class ColorSelection:
             self.color_picker (ColorPicker)
         """
         if self.color_type == ColorTypes.Mask:
-            self.color_icon_mask.icon_color = self.color_picker.color
+            self.color_icon_mask.icon_color = self.color_picker.colo
             self.config.set_mask_color(hex_to_rgb(self.color_picker.color))
             self.gui.canvas.mask_color = hex_to_rgb(self.color_picker.color)
             self.gui.canvas.update_mask_image()
