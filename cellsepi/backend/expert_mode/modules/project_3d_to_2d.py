48d13b04f04e4a5c194f93280e07988746ae87e9
ERIK-KE <erik-kubaczka@live.de>
2026-06-13 17:18:16 +0200

    Fixed issue with file loading in read_files module.
    Implemented subdirectory mechanism for expert mode. Needs to be refined to harmonize better with cache clearing.

--- cellsepi/backend/expert_mode/modules/project_3d_to_2d.py
+++ cellsepi/backend/expert_mode/modules/project_3d_to_2d.py
@@ -22,6 +22,9 @@ class Project3dTo2d(Module, ABC):
 
     def __init__(self, module_id: str = None) -> None:
         super().__init__(module_id)
+
+        # ToDo EK: Currently compatibility of ports is evaluated based on the key in this dictionary.
+        #  This effectively prevents multiple image_paths or mask_paths being connected to a single module
         self.inputs = {
             "image_paths": InputPort("image_paths", dict),
         }
@@ -30,12 +33,16 @@ class Project3dTo2d(Module, ABC):
         }
         self.user_projection_type: ProjectionType = ProjectionType.Z_MAX
 
-
     def run(self):
         images = self.inputs["image_paths"].data
         outputs_images = {}
         n_series = len(images)
-        self.event_manager.notify(ProgressEvent(percent=0, process=f"Projecting Series: Starting"))
+        self.event_manager.notify(
+            ProgressEvent(
+                percent=0,
+                process=f"Projecting Series: Starting"
+            )
+        )
         for iN, series in enumerate(images):
             if self.is_cancelled():
                 self.outputs["image_paths"].data = outputs_images
@@ -47,7 +54,7 @@ class Project3dTo2d(Module, ABC):
                 image_path = images[series][channel]
                 image = tifffile.imread(image_path)  # dimensions are: Z,Y,X
                 if image.ndim != 3:
-                    raise PipelineRunningException("Value Error","Wrong image format, expected a 3D image.")
+                    raise PipelineRunningException("Value Error", "Wrong image format, expected a 3D image.")
                 if self.user_projection_type == ProjectionType.Z_MAX:
                     projected = np.max(image, axis=0)
                     suffix = "_max"
@@ -55,18 +62,21 @@ class Project3dTo2d(Module, ABC):
                     projected = np.mean(image, axis=0).round().astype(image.dtype)
                     suffix = "_mean"
 
-                base_dir = os.path.dirname(image_path)
+                base_dir = self.get_working_directory()
                 name_without_type = os.path.splitext(os.path.basename(image_path))[0]
 
-                new_filename = f"{name_without_type}{suffix}.tif"
+                new_filename = f"{name_without_type}{suffix}.tiff"
                 new_path = os.path.join(base_dir, new_filename)
 
                 tifffile.imwrite(new_path, projected)
 
-
                 outputs_images[series][channel] = new_path
 
-            self.event_manager.notify(ProgressEvent(percent=int((iN + 1) / n_series * 100),
-                                                    process=f"Projecting Series: {iN + 1}/{n_series}"))
+            self.event_manager.notify(
+                ProgressEvent(
+                    percent=int((iN + 1) / n_series * 100),
+                    process=f"Projecting Series: {iN + 1}/{n_series}"
+                )
+            )
         self.outputs["image_paths"].data = outputs_images
         self.event_manager.notify(ProgressEvent(percent=100, process=f"Projecting Series: Finished"))
