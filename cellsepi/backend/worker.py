54c7c42035dfa2bf0550f1ff58c5b34c6a31b6f9
mmdark <fhock2002@gmail.com>
2026-06-23 16:21:30 +0200

    maybe fix macOS future error

--- cellsepi/backend/worker.py
+++ cellsepi/backend/worker.py
@@ -1,11 +1,22 @@
-import base64
-import os
-import sys
-import json
-import multiprocessing
 import __future__
 import timeit
+import math
+import typing
+import datetime
+import urllib
+import urllib.request
+import zipfile
+import sqlite3
+import pathlib
+import logging
+import uuid
 import ctypes
+import multiprocessing
+import json
+import sys
+import os
+import base64
+
 if os.name == "nt":
     import ctypes.wintypes
 
